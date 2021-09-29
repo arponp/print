0026435448e9502eda2e6d18d55855fa2934167d
Arpon Purkayastha <arpon@Arpons-MacBook-Pro.local>
2021-09-29 15:38:33 -0700

    print test pdf on local printer

--- src/print/Main.java
+++ src/print/Main.java
@@ -1,21 +1,20 @@
 package print;
 
-import java.io.*;
 import java.net.URI;
-
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		File file = new File("test.pdf");
-		URI destination = new URI("ipp://sunrise.intellinum.com/ipp/printer");
-		boolean printStatus = printFile(file,destination);
-		System.out.println(printStatus);
-		
-	}
-	
-	public static boolean printFile(File f, URI u) {
-		return false;
+		try { 
+			File testPDF = new File("test.pdf");
+			URI uri = new URI("http://localhost:631/printers/Brother_MFC_J775DW");
+			IppPrinter p = new IppPrinter(uri);
+			p.printJob(testPDF);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
