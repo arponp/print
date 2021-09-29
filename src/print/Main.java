f7d4bfabc7aa62fbe14f4c7467af8671b8bd1cf5
Arpon Purkayastha <arpon@Arpons-MacBook-Pro.local>
2021-09-28 21:32:54 -0700

    add CUPS dependency

--- src/print/Main.java
+++ src/print/Main.java
@@ -1,20 +1,21 @@
 package print;
 
-import java.awt.Desktop;
 import java.io.*;
+import java.net.URI;
+
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		File file = new File("test.pdf");
-		System.out.println(file);
-		if (Desktop.isDesktopSupported()) {
-			try {
-				Desktop.getDesktop().open(file);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		URI destination = new URI("ipp://sunrise.intellinum.com/ipp/printer");
+		boolean printStatus = printFile(file,destination);
+		System.out.println(printStatus);
+		
+	}
+	
+	public static boolean printFile(File f, URI u) {
+		return false;
 	}
 
 }
